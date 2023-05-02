37308b4526bd8f7f97e9e25152cd080e66118f9b
YashasXworkz <yashass.xworkz@gmail.com>
2023-05-02 20:59:11 +0530

    Adding ifelseif java related files

--- Car.java
+++ Car.java
@@ -1,14 +1,67 @@
 class Car {
-  static boolean isEngineOn; // false
-  static String brand = "Toyota";
-  static double price = 25000.00;
+  static boolean isEngineOn;
+  static String brand = "BMW";
+  static double price = 600000;
   static int maxSpeed = 120;
-  static int minSpeed;
+  static int minSpeed = 40;
   static int currentSpeed;
 
-  public static void main(String[] specs) {
-    System.out.println("Main Started");
+  public static boolean turnOnOrOffEngine() {
+    System.out.println("turnOnOrOffEngine method started");
+    if (isEngineOn) {
+      System.out.println("Engine is turned off......");
+      isEngineOn = false;
+    } else {
+      System.out.println("Engine is turned on......");
+      isEngineOn = true;
+    }
+    System.out.println("turnOnOrOffEngine method ended");
+    return isEngineOn;
+  }
+
+  public static int accelerate() {
+    System.out.println("accelerate method started");
+    if (isEngineOn) {
+      if (currentSpeed <= maxSpeed) {
+        currentSpeed += 10;
+        System.out.println("Current speed: " + currentSpeed + " mph");
+      }
+      if (currentSpeed == maxSpeed) {
+        System.out.println("Max speed reached.");
+      }
+    } else {
+      System.out.println("Turn on the engine first.");
+    }
+    System.out.println("accelerate method ended");
+    return currentSpeed;
+  }
+
+  public static int decelerate() {
+    System.out.println("decelerate method started");
+    if (isEngineOn) {
+      if (currentSpeed > minSpeed) {
+        currentSpeed -= minSpeed;
+        System.out.println("Current speed: " + currentSpeed + " mph");
+        if (currentSpeed == minSpeed) {
+          System.out.println("Min speed reached.");
+        }
+      }
+    } else {
+      System.out.println("Turn on the engine first.");
+    }
+    System.out.println("decelerate method ended");
+    return currentSpeed;
+  }
+
+  public static void main(String[] car) {
+    System.out.println("Main started");
     turnOnOrOffEngine();
+    accelerate();
+    accelerate();
+    accelerate();
+    accelerate();
+    accelerate();
+    accelerate();
     accelerate();
     accelerate();
     accelerate();
@@ -16,63 +69,10 @@ class Car {
     accelerate();
     accelerate();
     System.out.println("Engine is on: " + isEngineOn);
+    decelerate();
+    decelerate();
     turnOnOrOffEngine();
     accelerate();
-    decelerate();
-    System.out.println("Main Ended");
-  }
-
-  public static boolean turnOnOrOffEngine() {
-    System.out.println("----------------------------------------------");
-    System.out.println("turnOnOrOffEngine Method started");
-    // Conditional Statements
-    // false == false
-    if (isEngineOn == false) {
-      isEngineOn = true;
-      System.out.println("Engine is turned on......");
-    } else if (isEngineOn == true) {
-      isEngineOn = false;
-      System.out.println("Engine is turned off......");
-    }
-    System.out.println("turnOnOrOffEngine Method ended");
-    System.out.println("----------------------------------------------");
-    return isEngineOn;
-  }
-
-  public static int accelerate() {
-    System.out.println("accelerate Method started");
-    // false == true
-    if (isEngineOn == true) {
-      if (currentSpeed < maxSpeed) {
-        System.out.println("Current Speed is less than Max Speed. Proceeding to accelerate.");
-        currentSpeed = currentSpeed + 10;
-        System.out.println("The current speed is " + currentSpeed + " mph");
-      } else {
-        System.out.println("Max Speed is reached");
-      }
-    } else {
-      System.out.println("Turn on the Engine");
-    }
-    System.out.println("accelerate Method ended");
-    System.out.println("----------------------------------------------");
-    return currentSpeed;
-  }
-
-  public static int decelerate() {
-    System.out.println("decelerate Method started");
-    if (isEngineOn == true) {
-      if (minSpeed > currentSpeed) {
-        System.out.println("Current Speed is greater than Min Speed. Proceeding to decelerate.");
-        maxSpeed = maxSpeed - 10;
-        System.out.println("The current speed is " + maxSpeed + " mph");
-      } else {
-        System.out.println("Min Speed is reached");
-      }
-    } else {
-      System.out.println("Turn Off the Engine");
-    }
-    System.out.println("decelerate Method ended");
-    System.out.println("----------------------------------------------");
-    return minSpeed;
+    System.out.println("Main ended");
   }
 }
--- Television.java
+++ Television.java
@@ -1,81 +1,78 @@
 class Television {
-  static boolean isConnected; // false
+  static boolean isPowerOn;
   static String brand = "Sony";
-  static double price = 50000.00;
+  static double price = 30000;
   static int maxVolume = 100;
-  static int minVolume = 0;
-  static int currentVolume = 50;
+  static int minVolume = 20;
+  static int currentVolume;
 
-  public static void main(String[] args) {
-    System.out.println("Main Started");
-    turnOnOrOff();
-    increaseVolume();
-    increaseVolume();
-    increaseVolume();
-    increaseVolume();
-    increaseVolume();
-    increaseVolume();
-    increaseVolume();
-    increaseVolume();
-    increaseVolume();
-    increaseVolume();
-    increaseVolume();
-    System.out.println("TV is connected " + isConnected);
-    turnOnOrOff();
-    System.out.println("Main Ended");
-  }
-
-  public static boolean turnOnOrOff() {
-    System.out.println("----------------------------------------------");
-    System.out.println("turnOnOrOff Method is started");
-    // Conditional Statements
-    // false == false
-    if (isConnected == false) {
-      isConnected = true;
-      System.out.println("TV is turned on......");
-    } else if (isConnected == true) {
-      isConnected = false;
-      System.out.println("TV is turned off......");
+  public static boolean turnOnOrOffPower() {
+    System.out.println("turnOnOrOffPower method started");
+    if (isPowerOn) {
+      System.out.println("Television is turned off......");
+      isPowerOn = false;
+    } else {
+      System.out.println("Television is turned on......");
+      isPowerOn = true;
     }
-    System.out.println("turnOnOrOff Method is ended");
-    System.out.println("----------------------------------------------");
-    return isConnected;
+    System.out.println("turnOnOrOffPower method ended");
+    return isPowerOn;
   }
 
   public static int increaseVolume() {
-    System.out.println("increaseVolume Method is started");
-    // false == true
-    if (isConnected == true) {
+    System.out.println("increaseVolume method started");
+    if (isPowerOn) {
       if (currentVolume < maxVolume) {
-        System.out.println("Current Volume is less than Max Volume proceed");
-        currentVolume = currentVolume + 5;
-        System.out.println("The current volume is " + currentVolume);
-      } else {
-        System.out.println("Max Volume is reached");
+        currentVolume += 10;
+        System.out.println("Current volume: " + currentVolume);
+      }
+      if (currentVolume == maxVolume) {
+        System.out.println("Max volume reached.");
       }
     } else {
-      System.out.println("Turn on the TV");
+      System.out.println("Turn on the television first.");
     }
-    System.out.println("increaseVolume Method is ended");
-    System.out.println("----------------------------------------------");
+    System.out.println("increaseVolume method ended");
     return currentVolume;
   }
 
   public static int decreaseVolume() {
-    System.out.println("decreaseVolume Method is started");
-    if (isConnected == true) {
-      if (minVolume < currentVolume) {
-        System.out.println("Current Volume is greater than Min Volume");
-        currentVolume = currentVolume - 5;
-        System.out.println("The Current Volume is " + currentVolume);
-      } else {
-        System.out.println("Min Volume is reached");
+    System.out.println("decreaseVolume method started");
+    if (isPowerOn) {
+      if (currentVolume > minVolume) {
+        currentVolume -= minVolume;
+        System.out.println("Current volume: " + currentVolume);
+        if (currentVolume == minVolume) {
+          System.out.println("Min volume reached.");
+        }
       }
     } else {
-      System.out.println("Turn Off the TV");
+      System.out.println("Turn on the television first.");
     }
-    System.out.println("decreaseVolume Method is ended");
-    System.out.println("----------------------------------------------");
-    return minVolume;
+    System.out.println("decreaseVolume method ended");
+    return currentVolume;
+  }
+
+  public static void main(String[] television) {
+    System.out.println("Main started");
+    turnOnOrOffPower();
+    increaseVolume();
+    increaseVolume();
+    increaseVolume();
+    increaseVolume();
+    increaseVolume();
+    increaseVolume();
+    increaseVolume();
+    increaseVolume();
+    increaseVolume();
+    increaseVolume();
+    System.out.println("Television is on: " + isPowerOn);
+    decreaseVolume();
+    decreaseVolume();
+    decreaseVolume();
+    decreaseVolume();
+    turnOnOrOffPower();
+    increaseVolume();
+    System.out.println("Main ended");
   }
 }
