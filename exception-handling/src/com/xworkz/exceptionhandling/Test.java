851a3588ccfa60d60e3a0092f8ab6c77c659e3bf
YashasXworkz <yashass.xworkz@gmail.com>
2023-06-30 19:02:26 +0530

    Made Changes

---
+++ exception-handling/src/com/xworkz/exceptionhandling/Test.java
@@ -0,0 +1,13 @@
+package com.xworkz.exceptionhandling;
+
+public class Test {
+  String name;
+  
+  public Test(String name) {
+    this.name = name;
+  }
+  
+  public void display() {
+    System.out.println(name);
+  }
+}
